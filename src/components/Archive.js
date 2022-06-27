82deb4d0c6d772bb9265bc0a4377627b934a01cf
Timothy Rogers <github@hack13.me>
2022-06-27 14:06:17 -0400

    Updated thumbnail... because some reason link changed

--- src/components/Archive.js
+++ src/components/Archive.js
@@ -7,7 +7,7 @@ const Archive = () => {
             <br />
             <div class="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 p-3">
                 <div class="space-x-4 p-1 flex flex-row rounded-md border-2 border-solid border-stone-500">
-                <div><Link to="/game/22-06-25-s2"><img src="https://vz-f755a29c-81b.b-cdn.net/8f08142f-3c3e-4edf-87ff-0ed9469fb66c/thumbnail.jpg" class="w-16 md:w-32 lg:w-48 rounded-md" alt="video-thumbnail" /></Link></div>
+                <div><Link to="/game/22-06-25-s2"><img src="https://vz-f755a29c-81b.b-cdn.net/8f08142f-3c3e-4edf-87ff-0ed9469fb66c/thumbnail_c3d66236.jpg" class="w-16 md:w-32 lg:w-48 rounded-md" alt="video-thumbnail" /></Link></div>
                     <div class="text-serif">
                         <p class="text-2xl"><Link to="/game/22-06-25-s2">June 25, 2022 S2</Link></p>
                         <p><strong>Script:</strong> Seventh Son of a Seventh Son v2.1</p>
