a5f8bec382600c6edae885472a4f7dc9ef56b42e
Timothy Rogers <github@hack13.me>
2022-07-02 09:23:32 -0400

    fixed where wasn't setting effectRan

--- src/components/GameSession.js
+++ src/components/GameSession.js
@@ -23,14 +23,9 @@ const GameSession = () => {
                     setVidsrc(data["video sources"][0].source)
                 })
             }
-            console.log('effectchecked')
             fetchGameData(game.id)
-            return () => {
-                effectRan.current = true
-            }
+            effectRan.current = true
         }
-        
-        console.log('i did something')
     }, [game.id])
     console.log(gameData)
 
@@ -38,29 +33,37 @@ const GameSession = () => {
         setPlayer(type)
         setVidsrc(source)
     }
-
-    return (
-        <div class="bg-stone-100 flex flex-col rounded-md outline-3 p-4 space-y-4">
-            <div class="font-serif text-2xl text-stone-600">{!effectRan.current ? 'Loading' : gameData["game name"]}</div>
-                <VideoPlayer type={player} source={vidsrc} />
-            <div class="space-y-2 text-stone-600 text-serif">
-                <p class="text-xl font-bold">Video Sources</p>
-                <div class="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 p-3">
-                    {!effectRan.current ? 'Loading' : gameData["video sources"].map(video =>
-                        <button key={video.id} onClick={() => changePlayer(video.platform, video.source)} class="bg-slate-700 text-stone-200 rounded-md p-3"><strong>{video.platform}</strong> <br/> <strong>Perspective:</strong> {video.perspective} <br/> <strong>Credit:</strong> {video.credit}</button>
-                    )}
+    if (effectRan.current === true) {
+        return (
+            <div class="bg-stone-100 flex flex-col rounded-md outline-3 p-4 space-y-4">
+                <div class="font-serif text-2xl text-stone-600">{gameData && gameData["game name"]}</div>
+                    <VideoPlayer type={player} source={vidsrc} />
+                <div class="space-y-2 text-stone-600 text-serif">
+                    <p class="text-xl font-bold">Video Sources</p>
+                    <div class="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 p-3">
+                        {gameData["video sources"].map(video =>
+                            <button key={video.id} onClick={() => changePlayer(video.platform, video.source)} class="bg-slate-700 text-stone-200 rounded-md p-3"><strong>{video.platform}</strong> <br/> <strong>Perspective:</strong> {video.perspective} <br/> <strong>Credit:</strong> {video.credit}</button>
+                        )}
+                    </div> 
+                    <p class="text-xl font-bold">Story Tellers</p>
+                    <div class="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 p-3">
+                        {gameData.roster.storytellers.map(storyteller =>
+                            <StoryTeller key={storyteller.name} name={storyteller.name} pfp={storyteller.pfp} />
+                        )}
+                    </div>
+                    <p class="text-xl font-bold">Script</p>
+                        <ScriptRenderer key={gameData["game name"]} script={gameData.script} />
                 </div> 
-                <p class="text-xl font-bold">Story Tellers</p>
-                <div class="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 p-3">
-                    {!effectRan.current ? 'Loading' : gameData.roster.storytellers.map(storyteller =>
-                        <StoryTeller key={storyteller.name} name={storyteller.name} pfp={storyteller.pfp} />
-                    )}
-                </div>
-                <p class="text-xl font-bold">Script</p>
-                {!effectRan.current ? 'Loading' : <ScriptRenderer key={gameData["game name"]} script={gameData.script} />}
+            </div>
+        )
+    }else{
+        return(
+            <div class="bg-stone-100 flex flex-col rounded-md outline-3 p-4 space-y-4">
+            Loading...
             </div> 
-        </div>
-    )
+        )
+    }
+
 
 }
 
