ffb398df1f28c011fce54e659d75bedebabd0a00
Timothy Rogers <github@hack13.me>
2022-07-17 15:27:05 -0400

    Made roster more condensed

--- src/components/snippits/Roster.js
+++ src/components/snippits/Roster.js
@@ -14,7 +14,9 @@ const Roster = ({ residents }) => {
 
     return(
         <div class="bg-stone-100 flex flex-col rounded-md outline-3 p-4 border-2">
+            <div class="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 p-3">
             {rosterData.length === 0 ? 'No Roster Data Available' : rosterData}
+            </div>
         </div>
     )
 }
