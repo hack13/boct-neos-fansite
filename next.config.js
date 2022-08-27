fb5356df66fa70370211b8ede208c893e8253ab0
Timothy Rogers <github@hack13.me>
2022-08-27 09:53:43 -0400

    Prep work for trying Next's Image componet

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      "vz-f755a29c-81b.b-cdn.net"
+    ],
+    loader: "custom",
+    path: "/",
+  },
 }
 
 module.exports = nextConfig
